129fa7c6c5f8508256c84692c93ab27fd913be5f
kav <you@example.com>
2022-12-11 15:34:07 +0000

    Add easy toggle of nvim tree

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -15,5 +15,4 @@ map('n', '<C-j>', '<C-w><C-j>', {noremap=true})
 map('n', '<C-h>', '<C-w><C-h>', {noremap=true})
 map('n', '<C-k>', '<C-w><C-k>', {noremap=true})
 map('n', '<C-l>', '<C-w><C-l>', {noremap=true})
-
-
+map('n', '<leader>t', ':NvimTreeToggle<CR>', {noremap=true, silent=true})
