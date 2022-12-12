a75d14c03b48716d17320f0042ca9ff4d4a40786
Your Name <you@example.com>
2022-12-11 22:39:14 -0500

    Auto download packer

--- init.lua
+++ init.lua
@@ -1,7 +1,9 @@
-require('packages')
 if pcall(require, 'packer') then
+  require('packages')
   vim.cmd('PackerInstall')
   require('config')
+else
+  os.execute('git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim')
 end
 require('settings')
 require('mappings')
